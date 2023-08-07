b88f5fd846465e2306372377a293b555f16b8046
SoroushGReza <mycodeschool22@outlook.com>
2023-08-07 11:19:01 +0000

    Created: 'Movie' model
    Added: 'movies' to settings.py 'INSTALLED_APPS'

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+# Movie model
+class Movie(models.Model):
+    title = models.CharField(max_length=200)  # Movie title
+    genre = models.CharField(max_length=100)  # Movie Genre
+    release_date = models.DateField()  # Movie Release date
+    rating = models.DecimalField(max_digits=5, decimal_places=1)  # Rating
+    description = models.TextField()  # Movie Description
