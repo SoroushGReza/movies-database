173df4d6aca4a0fa469016ea96200388fbd443bb
SoroushGReza <mycodeschool22@outlook.com>
2023-08-07 11:39:53 +0000

    Created: 'Review' Model

--- movies/models.py
+++ movies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Movie model
@@ -8,3 +9,17 @@ class Movie(models.Model):
     release_date = models.DateField()  # Movie Release date
     rating = models.DecimalField(max_digits=5, decimal_places=1)  # Rating
     description = models.TextField()  # Movie Description
+
+
+# Review Model
+class Review(models.Model):
+    movie = models.ForeignKey(
+        Movie, on_delete=models.CASCADE, related_name='reviews'
+    )
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reviews'
+    )
+    review_text = models.TextField(
+        max_length=1000, help_text="Enter your review here."
+    )
+    date = models.DateField(auto_now_add=True)
