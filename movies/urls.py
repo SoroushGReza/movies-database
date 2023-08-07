07dc9b2c99cff0da4bc13d32ef70c7196cc58464
SoroushGReza <mycodeschool22@outlook.com>
2023-08-07 14:14:01 +0000

    Added: movie_detail view and template

--- movies/urls.py
+++ movies/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.movie_list, name='movie_list'),
+    path('<int:pk>/', views.movie_detail, name='movie_detail'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import Movie, Genre, Review
+from django.shortcuts import render, get_object_or_404
 
 
 def movie_list(request):
     movies = Movie.objects.all()
     return render(request, 'movies/movie_list.html', {'movies': movies})
+
+
+def movie_detail(request, pk):
+    movie = get_object_or_404(Movie, pk=pk)
+    return render(request, 'movies/movies_detail.html', {'movie': movie})
