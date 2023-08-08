4bd1366b5c555ba5e8521ecc0e3ff0758e7439c5
SoroushGReza <mycodeschool22@outlook.com>
2023-08-08 06:25:25 +0000

    Fix: Removed double-quotes from 'TMDB_API_KEY' to solve KeyError (SUCCESS)

--- movies/tmdb.py
+++ movies/tmdb.py
@@ -4,6 +4,6 @@ from .env import TMDB_API_KEY
 
 def get_movies_from_tmdb():
     url = "https://api.themoviedb.org/3/movie/popular"
-    querystring = {"api_key": "TMDB_API_KEY", "language": "en-US", "page": "1"}
+    querystring = {"api_key": TMDB_API_KEY, "language": "en-US", "page": "1"}
     response = requests.request("GET", url, params=querystring)
     return response.json()
