b4259d6ad1e567ca079a45a87da0686a7a3bc848
SoroushGReza <mycodeschool22@outlook.com>
2023-08-08 09:27:25 +0000

    Link: 'register.html' to 'style.css' and python (UNSUCCESSFULL)

--- movie_database/settings.py
+++ movie_database/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
