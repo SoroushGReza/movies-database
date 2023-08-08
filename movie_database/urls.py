63e4d9acbf6a8d007fb65a4fd3d218c87c73bd17
SoroushGReza <mycodeschool22@outlook.com>
2023-08-08 18:44:09 +0000

    Redirect root to login page

--- movie_database/urls.py
+++ movie_database/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from movies.views import user_login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/', include('movies.urls')),
+    re_path(r'^$', user_login, name='home'),
 ]
