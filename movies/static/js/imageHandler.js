6f85df592938a5bdfce3de5792214e9e1fd696ea
SoroushGReza <mycodeschool22@outlook.com>
2023-08-15 08:14:21 +0000

    Testing / Experimenting: trying to solve issue of fallback image not being show, added data-fallback-image attribute to img element in movie_list.html (Status: Issue not resolved

--- movies/static/js/imageHandler.js
+++ movies/static/js/imageHandler.js
@@ -1,6 +1,6 @@
 // Image error handler
 function handleImageError(imageElement) {
     imageElement.onerror = null;
-    imageElement.src = '{% static "media/image404.png" %}';
+    imageElement.src = document.body.getAttribute('data-fallback-image');
     imageElement.classList.add('fallback-image');
 }
--- movies/static/js/profile.js
+++ movies/static/js/profile.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('edit-profile-button').addEventListener('click', function() {
-        var form = document.getElementById('edit-profile-form');
-        form.style.display = form.style.display === 'none' ? 'block' : 'none';
-    });
+    var editProfileButton = document.getElementById('edit-profile-button');
+    if (editProfileButton) { // Check if the element exists
+        editProfileButton.addEventListener('click', function() {
+            var form = document.getElementById('edit-profile-form');
+            form.style.display = form.style.display === 'none' ? 'block' : 'none';
+        });
+    } else {
+        console.warn('Edit profile button not found'); // Warning if element is not found
+    }
 });
