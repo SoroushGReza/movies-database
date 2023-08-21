5d5291620266e3eee3a4007606821cccf537124e
SoroushGReza <mycodeschool22@outlook.com>
2023-08-21 15:35:41 +0000

    Imported Review from models

--- movies/admin.py
+++ movies/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Movie
+from .models import Movie, Review
 
 
 class MovieAdmin(admin.ModelAdmin):
@@ -7,3 +7,4 @@ class MovieAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Movie, MovieAdmin)
+admin.site.register(Review)
