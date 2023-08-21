10ba998813f7d2cc83f06ae09fd407acab5dbb5b
SoroushGReza <mycodeschool22@outlook.com>
2023-08-21 15:09:03 +0000

    Deleted existing Review, UserList and Rating models in preparation for TDD approach.

--- movies/models.py
+++ movies/models.py
@@ -12,45 +12,6 @@ class Movie(models.Model):
     description = models.TextField()  # Movie Description
 
 
-# Review Model
-class Review(models.Model):
-    movie = models.ForeignKey(
-        Movie, on_delete=models.CASCADE, related_name='reviews'
-    )
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='reviews'
-    )
-    review_text = models.TextField(
-        max_length=1000, help_text="Enter your review here."
-    )
-    date = models.DateField(auto_now_add=True)
-
-
-# Rating Model
-class Rating(models.Model):
-    movie = models.ForeignKey(
-        Movie, on_delete=models.CASCADE, related_name='ratings'
-    )
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='ratings'
-    )
-    rating_value = models.IntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(5)]
-    )  # Rating values between 1 to 5
-    date = models.DateTimeField(auto_now_add=True)
-
-
-# User list  Model
-class UserList(models.Model):
-    """
-    Represents Users list of movies
-    """
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='lists'
-    )
-    movies = models.ManyToManyField(Movie, related_name='lists')
-
-
 # Movie Genre
 class Genre(models.Model):
     name = models.CharField(max_length=100)
