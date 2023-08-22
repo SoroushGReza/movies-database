a2b1ec4c54035b9e3550262ee6c95e75609338b2
SoroushGReza <mycodeschool22@outlook.com>
2023-08-22 09:23:14 +0000

    Created: ReviewFormTest that fails to build the code for it next

--- movies/test_models.py
+++ movies/test_models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from .models import Review
 
-
+# Review Model Test
 class ReviewModelTest(TestCase):
     def setUp(self):
         self.user = User.objects.create(username='testuser', password='12345')
@@ -11,3 +11,13 @@ class ReviewModelTest(TestCase):
         review = Review.objects.create(user=self.user, movie_id=12345, text="Great movie!", rating=4.5)
         self.assertEqual(review.text, "Great movie!")
         self.assertEqual(review.rating, 4.5)
+
+
+# Review Form Test 
+class ReviewFormTest(TestCase):
+    def test_review_form_in_movie_overview(self):
+        movie_id = 12345
+        response = self.client.get(reverse('movie_overview', args=[movie_id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Submit Review')
+        self.assertTemplateUsed(response, 'movies/movie_overview.html')
