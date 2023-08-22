8b847c05b77f464a936ce2a8fac3fa4b61365913
SoroushGReza <mycodeschool22@outlook.com>
2023-08-22 05:47:10 +0000

    Deleted: MovieAdmin class and unregistered Movie model from admin panel"

--- movies/admin.py
+++ movies/admin.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
-from .models import Movie, Review
+from .models import Review
 
 
-class MovieAdmin(admin.ModelAdmin):
-    list_filter = ('release_date',)  # Fields you want to filter by
-
-
-admin.site.register(Movie, MovieAdmin)
 admin.site.register(Review)
