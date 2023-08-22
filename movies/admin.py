13e9f25362ddbd350c1ff8f6382f1970a33d41d1
SoroushGReza <mycodeschool22@outlook.com>
2023-08-22 16:18:54 +0000

    Added: review managment in admin panel

--- movies/admin.py
+++ movies/admin.py
@@ -1,1 +1,20 @@
 from django.contrib import admin
+from .models import Review
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['user', 'movie_id', 'text', 'status']
+    list_filter = ('status', 'approved')
+    actions = ['approve_reviews', 'reject_reviews']
+
+    # Approving Reviews
+    def approve_reviews(self, request, queryset):
+        queryset.update(status='approved', approved=True)
+
+    # Rejecting reviews
+    def reject_reviews(self, request, queryset):
+        queryset.update(status='approved', approved=False)
+
+    approve_reviews.short_description = "Approve selected reviews"
+    reject_reviews.short_description = "Reject selected reviews"
