9ac95776a77cb0f8391e4dc8aaa2e345c4045c05
SoroushGReza <mycodeschool22@outlook.com>
2023-08-22 07:59:16 +0000

    Added: test for Review model creation including rating and text in test_models.py

--- movies/test_models.py
+++ movies/test_models.py
@@ -1,2 +1,13 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
+from .models import Review
+
+
+class ReviewModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser', password='12345')
+
+    def test_review_creation(self):
+        review = Review.objects.create(user=self.user, movie_id=12345, text="Great movie!", rating=4.5)
+        self.assertEqual(review.text, "Great movie!")
+        self.assertEqual(review.rating, 4.5)
