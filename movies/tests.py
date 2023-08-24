f8b0ee764cafb3eeb8a07cbe8387bf9ded756a58
SoroushGReza <mycodeschool22@outlook.com>
2023-08-24 10:32:33 +0000

    Added: movie_id to the id of the movie Blow

--- movies/tests.py
+++ movies/tests.py
@@ -11,7 +11,7 @@ class ReviewModelTest(TestCase):
 
     def test_review_creation(self):
         review = Review.objects.create(
-            user=self.user, movie_id=12345, text="Great movie!", rating=4.5
+            user=self.user, movie_id=4133, text="Great movie!", rating=4.5
         )
         self.assertEqual(review.text, "Great movie!")
         self.assertEqual(review.rating, 4.5)
