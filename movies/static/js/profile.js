8ab33b22393163be60672c7c25689f276d83c838
SoroushGReza <mycodeschool22@outlook.com>
2023-08-27 09:33:19 +0000

    Added: window on load to solve console warning

--- movies/static/js/profile.js
+++ movies/static/js/profile.js
@@ -1,11 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var editProfileButton = document.getElementById('edit-profile-button');
-    if (editProfileButton) { // Check if the element exists
-        editProfileButton.addEventListener('click', function() {
-            var form = document.getElementById('edit-profile-form');
-            form.style.display = form.style.display === 'none' ? 'block' : 'none';
-        });
-    } else {
-        console.warn('Edit profile button not found'); // Warning if element is not found
-    }
-});
+window.onload = function () {
+    document.addEventListener('DOMContentLoaded', function () {
+        var editProfileButton = document.getElementById('edit-profile-button');
+        if (editProfileButton) { // Check if the element exists
+            editProfileButton.addEventListener('click', function () {
+                var form = document.getElementById('edit-profile-form');
+                form.style.display = form.style.display === 'none' ? 'block' : 'none';
+            });
+        } else {
+            console.warn('Edit profile button not found'); // Warning if element is not found
+        }
+    });
+};
