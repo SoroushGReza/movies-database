4c1ac4b994b099a4c1262d6ab7b5553acb57477f
SoroushGReza <mycodeschool22@outlook.com>
2023-09-01 13:39:13 +0000

    Implemented: AJAX to delete reviews

--- movies/static/js/profile.js
+++ movies/static/js/profile.js
@@ -9,3 +9,35 @@ document.addEventListener('DOMContentLoaded', function () {
         console.warn('Edit profile button not found'); // Warning if element is not found
     }
 });
+
+// Detele user reviews confirmation
+document.addEventListener("DOMContentLoaded", function () {
+    const deleteButtons = document.querySelectorAll('.btn-danger');
+    deleteButtons.forEach(function (button) {
+        button.addEventListener('click', function (event) {
+            event.preventDefault(); // Prevent default
+            const confirmation = window.confirm('Are you sure you want to delete this review?');
+            if (confirmation) {
+                const reviewId = event.target.getAttribute('data-review-id');
+                const url = `/movies/delete_review/${reviewId}/`;
+                fetch(url, {
+                        method: 'POST',
+                        headers: {
+                            'X-Requested-With': 'XMLHttpRequest',
+                            'X-CSRFToken': csrftoken
+                        },
+                    })
+                    .then(response => response.json())
+                    .then(data => {
+                        if (data.status === 'success') {
+                            event.target.closest('.review-item').remove();
+                        }
+                    });
+                // Perform delete operation
+                console.log('User confirmed deletion');
+            } else {
+                console.log('User canceled deletion');
+            }
+        });
+    });
+});
