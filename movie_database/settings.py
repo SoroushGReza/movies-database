9908428cc17d37ed58ce2c715f09f54488f6e2d2
SoroushGReza <mycodeschool22@outlook.com>
2023-09-10 13:16:28 +0000

    Corrected: import for EMAIL_HOST_USER and EMAIL_HOST_PASSWORD

--- movie_database/settings.py
+++ movie_database/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
-from .env import EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
+from movies.env import EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
 from pathlib import Path
 
 
