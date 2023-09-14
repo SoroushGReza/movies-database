f8136a51779b1c242b117af15e93ec46897baf4b
SoroushGReza <mycodeschool22@outlook.com>
2023-09-14 11:58:46 +0000

    Updated database settings in env.py and settings.py

--- movie_database/settings.py
+++ movie_database/settings.py
@@ -97,8 +97,10 @@ WSGI_APPLICATION = 'movie_database.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
     }
 }
 
