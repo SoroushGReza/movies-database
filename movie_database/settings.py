8a7795df7d5e333bed89645ade4950e974cd6c2b
SoroushGReza <mycodeschool22@outlook.com>
2023-09-14 10:44:28 +0000

    Deleted: whitespace

--- movie_database/settings.py
+++ movie_database/settings.py
@@ -162,3 +162,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+django_heroku.settings(locals())
