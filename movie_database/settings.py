687b2f4561728bb6799abc6bb3e9fa189ed3e07a
SoroushGReza <mycodeschool22@outlook.com>
2023-09-14 10:43:41 +0000

    Added: django Heroku inegration for Heroku doployment

--- movie_database/settings.py
+++ movie_database/settings.py
@@ -13,10 +13,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from movies.env import EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
 from pathlib import Path
+import django_heroku
 
 
-# Email verification
-EMAIL_VERIFICATION_URL = 'verify_email'
+# Welcome Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
