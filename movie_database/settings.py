c1bc3f8577f5fd30ad124c4be6fd926d7b8709e9
SoroushGReza <mycodeschool22@outlook.com>
2023-09-15 16:00:11 +0000

    Set Debug to True

--- movie_database/settings.py
+++ movie_database/settings.py
@@ -45,7 +45,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'movies/templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-soroushgrez-moviesdatab-21but2m2le2.ws-us104.gitpod.io',
