4ff6973db9406eb72700cd8ff3fee06e5b2ac785
SoroushGReza <mycodeschool22@outlook.com>
2023-09-16 10:47:20 +0000

    Deleted: compromised  EMAIL_HOST_PASSWORD, updated to new password and using the variable from env.py instead

--- movie_database/settings.py
+++ movie_database/settings.py
@@ -33,7 +33,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'movie.base.247@gmail.com'
-EMAIL_HOST_PASSWORD = 'tatxuskrypnhssnv'
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
 
 LOGIN_URL = 'movies:user_login'
 
