2d6853c229b92507088b333e3fb15c0855e56a75
SoroushGReza <mycodeschool22@outlook.com>
2023-09-21 14:21:31 +0000

    1. Implemented: checkWindowSize function to toggle navbar style 2. Added: event listeners for page load and resizing events

---
+++ movies/static/js/navbar.js
@@ -0,0 +1,47 @@
+// Check window size and apply changes according to size
+function checkWindowSize() {
+    // Get element with id "navbarDropdown"
+    const linkElement = document.getElementById("navbarDropdown");
+    
+    // Get hamburger menu button
+    const hamburgerElement = document.querySelector(".navbar-toggler");
+    
+    // Get custom icon for toggler button
+    const customTogglerIcon = document.getElementById("custom-toggler-icon"); 
+    
+    // Define media query for screen width between 768px and 991px
+    const mediaQuery = window.matchMedia("(min-width: 768px) and (max-width: 991px)");
+
+    // Check if media query matches
+    if (mediaQuery.matches) {
+        // Set the text of the link elementt to "Movie Base"
+        linkElement.textContent = "Movie Base";
+        
+        // Add class "movie-base-style" to style in css
+        linkElement.classList.add("movie-base-style");
+        
+        // Set text of the custom toggler icon to "Movie Base"
+        customTogglerIcon.textContent = "Movie Base";  
+        
+        // Add class "movie-base-style" to style it in css
+        customTogglerIcon.classList.add("movie-base-style");
+    } else {
+        // Set the text of the link element to "Profile"
+        linkElement.textContent = "Profile";
+        
+        // Remove the class "movie-base-style"
+        linkElement.classList.remove("movie-base-style");
+        
+        // Set the text of the custom toggler icon to "Movie Base"
+        customTogglerIcon.textContent = "Movie Base";
+        
+        // Remove the class "movie-base-style"
+        customTogglerIcon.classList.remove("movie-base-style");
+    }
+}
+
+// Run the checkWindowSize function when the page loads
+window.addEventListener("load", checkWindowSize);
+
+// Run the checkWindowSize function whenever the window size changes
+window.addEventListener("resize", checkWindowSize);
