13dedd32d2cb0ed50f48f477fec2560cb849bc5c
SoroushGReza <mycodeschool22@outlook.com>
2023-09-22 09:33:34 +0000

    Deleted: LOGGING, since I had duplicates of it

--- movie_database/settings.py
+++ movie_database/settings.py
@@ -59,27 +59,6 @@ CSRF_TRUSTED_ORIGINS = [
 
 CSRF_COOKIE_NAME = 'csrftoken'
 
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-        },
-    },
-    "root": {
-        "handlers": ["console"],
-        "level": "WARNING",
-    },
-    "loggers": {
-        "django": {
-            "handlers": ["console"],
-            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
-            "propagate": False,
-        },
-    },
-}
-
 
 # Application definition
 
