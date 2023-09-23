5e2c158daf492205d1f6b210fdd4b8162b310c17
SoroushGReza <mycodeschool22@outlook.com>
2023-09-23 16:58:54 +0000

    Declared: handleImageError as exported and add 'use strict' for JsHint validation

--- movies/static/js/imageHandler.js
+++ movies/static/js/imageHandler.js
@@ -1,5 +1,7 @@
+/* exported handleImageError */
 // Image error handler
 function handleImageError(imageElement) {
+    'use strict';
     imageElement.onerror = null;
     imageElement.src = imageElement.getAttribute('data-fallback-image');
     imageElement.classList.add('fallback-image');
