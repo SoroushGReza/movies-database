8621e8486e1b2c167f69d0ca5d1af0b66fe6ef89
SoroushGReza <mycodeschool22@outlook.com>
2023-09-23 16:49:35 +0000

    Added:  'use strict' and declare emailjs as global to pass JSHint validation

--- movies/static/js/contact.js
+++ movies/static/js/contact.js
@@ -1,26 +1,29 @@
-// Initialise EmailJS with user ID
-(function(){
+/* global emailjs */
+(function () {
+    'use strict';
+    // Initialise EmailJS with user ID
     emailjs.init("9HmLA-YG0nrtPgthk");
-})();
+}());
 
-document.getElementById('contact-form').addEventListener('submit', function(event) {
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    'use strict';
     event.preventDefault();
 
     // Get form data
-    var from_name = document.getElementById('name').value;
-    var from_email = document.getElementById('email').value;
-    var message = document.getElementById('message').value;
+    var from_name = document.getElementById('name').value,
+        from_email = document.getElementById('email').value,
+        message = document.getElementById('message').value;
 
     // Send the form via EmailJS
     emailjs.send('service_hry28h6', 'template_doij8ju', {
         from_name: from_name,
         from_email: from_email,
-        message: message,
+        message: message
     })
-    .then(function() {
+    .then(function () {
         // Alert the user that the message has been sent
         alert('Message sent!');
-    }, function(error) {
+    }, function (error) {
         // Alert the user in case of an error
         alert('Sorry... ' + JSON.stringify(error));
     });
