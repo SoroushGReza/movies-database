dea547b429e2116da4f7afd108705031772aa299
SoroushGReza <mycodeschool22@outlook.com>
2023-09-23 17:05:31 +0000

    JsHint Validation: Add ES6 and 'use strict' declarations, remove unused variable

--- movies/static/js/navbar.js
+++ movies/static/js/navbar.js
@@ -1,40 +1,40 @@
+/* jshint esversion: 6 */
+'use strict';
+
 // Check window size and apply changes according to size
 function checkWindowSize() {
     // Get element with id "navbarDropdown"
     const linkElement = document.getElementById("navbarDropdown");
-    
-    // Get hamburger menu button
-    const hamburgerElement = document.querySelector(".navbar-toggler");
-    
+
     // Get custom icon for toggler button
-    const customTogglerIcon = document.getElementById("custom-toggler-icon"); 
-    
+    const customTogglerIcon = document.getElementById("custom-toggler-icon");
+
     // Define media query for screen width between 768px and 991px
     const mediaQuery = window.matchMedia("(min-width: 768px) and (max-width: 991px)");
 
     // Check if media query matches
     if (mediaQuery.matches) {
-        // Set the text of the link elementt to "Movie Base"
+        // Set the text of the link element to "Movie Base"
         linkElement.textContent = "Movie Base";
-        
-        // Add class "movie-base-style" to style in css
+
+        // Add class "movie-base-style" to style in CSS
         linkElement.classList.add("movie-base-style");
-        
+
         // Set text of the custom toggler icon to "Movie Base"
-        customTogglerIcon.textContent = "Movie Base";  
-        
-        // Add class "movie-base-style" to style it in css
+        customTogglerIcon.textContent = "Movie Base";
+
+        // Add class "movie-base-style" to style it in CSS
         customTogglerIcon.classList.add("movie-base-style");
     } else {
         // Set the text of the link element to "Profile"
         linkElement.textContent = "Profile";
-        
+
         // Remove the class "movie-base-style"
         linkElement.classList.remove("movie-base-style");
-        
+
         // Set the text of the custom toggler icon to "Movie Base"
         customTogglerIcon.textContent = "Movie Base";
-        
+
         // Remove the class "movie-base-style"
         customTogglerIcon.classList.remove("movie-base-style");
     }
